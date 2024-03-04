9142ed58140352e628d933ea6b0fe88627593b75
Swetha Janardhana <SwethaJanardhana85@gmail.com>
2024-03-04 09:25:25 +0100

    added Back link

--- components/ArtPieceDetails/index.js
+++ components/ArtPieceDetails/index.js
@@ -1,5 +1,11 @@
 import { styled } from "styled-components";
 import Image from "next/image";
+import Link from "next/link";
+import LeftArrow from "@/public/assets/left-arrow.svg";
+
+const StyledLeftArrow = styled(LeftArrow)`
+  width: 2rem;
+`;
 
 const StyledImage = styled(Image)`
   object-fit: contain;
@@ -10,6 +16,9 @@ const StyledImage = styled(Image)`
 export default function ArtPieceDetails({ image, title, artist, year, genre }) {
   return (
     <>
+      <Link href="/art-pieces">
+        <StyledLeftArrow />
+      </Link>
       <h2>{title}</h2>
       <p>{artist}</p>
       <StyledImage src={image} alt={title} width={100} height={100} />
